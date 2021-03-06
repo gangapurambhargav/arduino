87539d403a2c62f409d87bd5d09683f4737942f0
Gangapuram Bhargav <gangapurambhargav1@gmail.com>
2021-03-07 00:20:14 +0530

    2nd

---
+++ code_ino.ino
@@ -0,0 +1,27 @@
+
+int wire = 2;
+int led = 4;
+int buzzer = 3;
+
+void setup() {
+
+  Serial.begin(9600);
+  pinMode(led,OUTPUT);
+  pinMode(buzzer,OUTPUT);
+  pinMode(wire, INPUT);
+  digitalWrite(led,LOW);
+  digitalWrite(buzzer,LOW);
+}
+
+void loop() {
+
+  int buttonState = digitalRead(wire);
+  if(buttonState==1)
+  {
+    digitalWrite(led,HIGH);
+     digitalWrite(buzzer,HIGH);
+  }
+
+  Serial.println(buttonState);
+         
+}
